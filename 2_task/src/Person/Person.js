94e09bf3c532a604a3a7d23bd43138a9336595b5
bslzz <bishalkoid@gmail.com>
2020-03-25 13:24:12 +0200

    person component added and styled

---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Person.css';
+
+const Person = () => {
+  return (
+    <div className="personcard">
+      <h1>Hello, My name is Bishal Aryal.</h1>
+      <p>I am a student</p>
+    </div>
+  );
+};
+
+export default Person;
