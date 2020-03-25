03eccfe4baf1a76218ce031f10a8ad511ad8a134
bslzz <bishalkoid@gmail.com>
2020-03-25 14:29:10 +0200

    changed props and added states in app.js

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = () => {
-  return (
-    <div>
-      <h1> Hello World</h1>
-      <p> Hello Everyone</p>
-      <Person name="Bishal" gender="male" />
-      <Person name="Eerika" gender="female" />
-      <Person name="Alex" gender="male" />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Bishal Aryal', gender: 'male' },
+      { name: 'Eerika', gender: 'female' },
+      { name: 'Alex Hello', gender: 'male' }
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> Hello World</h1>
+        <p> Hello Everyone</p>
+        <Person
+          name={this.state.persons[0].name}
+          gender={this.state.persons[0].gender}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          gender={this.state.persons[1].gender}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          gender={this.state.persons[2].gender}
+        />
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1> Hello World</h1>
+//       <p> Hello Everyone</p>
+//       <Person name="Bishal" gender="male" />
+//       <Person name="Eerika" gender="female" />
+//       <Person name="Alex" gender="male" />
+//     </div>
+//   );
+// };
 
 export default App;
