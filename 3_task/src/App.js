6e546653ce4cc9468b283fc08fdc8be4c84c5369
bslzz <bishalkoid@gmail.com>
2020-03-30 11:36:26 +0300

    added subtractHandler and resetHandler buttons for functionality

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -7,8 +7,13 @@ class App extends Component {
   };
 
   addHandler = () => {
-    console.log('test');
     this.setState({ likes: this.state.likes + 1 });
+  };
+  subtractHandler = () => {
+    this.setState({ likes: this.state.likes - 1 });
+  };
+  resetHandler = () => {
+    this.setState({ likes: (this.state.likes = 0) });
   };
 
   render() {
@@ -16,6 +21,8 @@ class App extends Component {
       <div>
         <h1>Total likes: {this.state.likes}</h1>
         <button onClick={this.addHandler}>Add like</button>
+        <button onClick={this.subtractHandler}>Subtract like</button>
+        <button onClick={this.resetHandler}>Reset</button>
       </div>
     );
   }
