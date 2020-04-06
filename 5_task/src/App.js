b9de2a0c4b87b95c1456510e5c702e381a60c6f3
bslzz <bishalkoid@gmail.com>
2020-04-06 12:27:39 +0300

    added header and styled css for app.js component

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -1,4 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="mygame">
+        <div className="header">
+          <h1>Speedtest Game</h1>
+          <input type="text" placeholder="Your Score" />
+        </div>
+        <div className="buttons">
+          <button>Start Game</button>
+          <button>End Game</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
