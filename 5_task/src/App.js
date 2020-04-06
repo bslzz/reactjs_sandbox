a31b9aa43c5fe86daeb120126f6fc4f7e2a780d2
bslzz <bishalkoid@gmail.com>
2020-04-06 15:57:14 +0300

    added colors to the circle and worked with functionality

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -2,14 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 import Circle from './Circle/Circle.js';
 
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 class App extends Component {
   state = {
     score: 0,
+    current: 0,
   };
+
+  circleSpeed = 1500;
+  timer = undefined;
+
+  //Finding the next random circle
+
+  next = () => {
+    let nextActive = undefined;
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+    });
+    this.timer = setTimeout(this.next, this.circleSpeed);
+    console.log(this.state.current);
+  };
+
+  //finding a random number
 
   clickHandler = (circleNumber) => {
     console.log('Clicked', circleNumber);
     this.setState({ score: this.state.score + 1 });
+  };
+
+  startHandler = () => {
+    this.next();
+  };
+  endHandler = () => {
+    clearTimeout(this.timer);
   };
 
   render() {
@@ -20,14 +52,30 @@ class App extends Component {
           <p>Your score: {this.state.score}</p>
         </div>
         <main>
-          <Circle clicked={this.clickHandler.bind(this, 1)} />
-          <Circle clicked={this.clickHandler.bind(this, 2)} />
-          <Circle clicked={this.clickHandler.bind(this, 3)} />
-          <Circle clicked={this.clickHandler.bind(this, 4)} />
+          <Circle
+            active={this.state.current === 1}
+            buttonColor="yellow"
+            clicked={this.clickHandler.bind(this, 1)}
+          />
+          <Circle
+            active={this.state.current === 2}
+            buttonColor="green"
+            clicked={this.clickHandler.bind(this, 2)}
+          />
+          <Circle
+            active={this.state.current === 3}
+            buttonColor="pink"
+            clicked={this.clickHandler.bind(this, 3)}
+          />
+          <Circle
+            active={this.state.current === 4}
+            buttonColor="brown"
+            clicked={this.clickHandler.bind(this, 4)}
+          />
         </main>
         <div className="buttons">
-          <button>Start Game</button>
-          <button>End Game</button>
+          <button onClick={this.startHandler}>Start Game</button>
+          <button onClick={this.endHandler}>End Game</button>
         </div>
       </div>
     );
--- 5_task/src/Circle/Circle.js
+++ 5_task/src/Circle/Circle.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './Circle.css';
 
 const Circle = (props) => {
-  return <div className="circle" onClick={props.clicked}></div>;
-};
+  return (
+    <div
+      className={'circle' + (props.active ? ' active' : ' ')}
+      onClick={props.clicked}
+      style={{
+        backgroundColor: props.active ? props.active : props.buttonColor,
+      }}
+    ></div>
+  );
+}; /* clicked can be named anything as u wish and should be passed to app.js*/
+
+// ? = if
+// : = else, if not
 
 export default Circle;
