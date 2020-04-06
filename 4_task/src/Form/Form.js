3972a94b06c89729c8d849e53379b2f520ec8f19
bslzz <bishalkoid@gmail.com>
2020-04-06 11:45:46 +0300

    4_task ready

--- 4_task/src/Form/Form.js
+++ 4_task/src/Form/Form.js
@@ -5,16 +5,16 @@ const Form = (props) => {
   return (
     <form>
       <label htmlFor="fname">First name</label>
-      <input type="text" name="firstname" onChange={props.changeHandler} />
+      <input type="text" name="firstname" onChange={props.onChange} />
       <br />
       <label htmlFor="lname">Last name</label>
-      <input type="text" name="lastname" onChange={props.changeHandler} />
+      <input type="text" name="lastname" onChange={props.onChange} />
       <br />
       <label htmlFor="number">Phone number</label>
-      <input type="number" name="phone" onChange={props.changeHandler} />
+      <input type="number" name="phone" onChange={props.onChange} />
       <br />
       <label htmlFor="message">Message</label>
-      <textarea name="message" onChange={props.changeHandler}></textarea>
+      <textarea name="message" onChange={props.onChange}></textarea>
     </form>
   );
 };
