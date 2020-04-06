53d3e966b85c90670c39bedadbc09c369f175422
bslzz <bishalkoid@gmail.com>
2020-04-06 13:21:53 +0300

    clickhandler added, passed circle number in console.log and updated score placeholder

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -3,18 +3,27 @@ import './App.css';
 import Circle from './Circle/Circle.js';
 
 class App extends Component {
+  state = {
+    score: 0,
+  };
+
+  clickHandler = (circleNumber) => {
+    console.log('Clicked', circleNumber);
+    this.setState({ score: this.state.score + 1 });
+  };
+
   render() {
     return (
       <div className="mygame">
         <div className="header">
           <h1>Speedtest Game</h1>
-          <p>Your score: 0</p>
+          <p>Your score: {this.state.score}</p>
         </div>
         <main>
-          <Circle />
-          <Circle />
-          <Circle />
-          <Circle />
+          <Circle clicked={this.clickHandler.bind(this, 1)} />
+          <Circle clicked={this.clickHandler.bind(this, 2)} />
+          <Circle clicked={this.clickHandler.bind(this, 3)} />
+          <Circle clicked={this.clickHandler.bind(this, 4)} />
         </main>
         <div className="buttons">
           <button>Start Game</button>
