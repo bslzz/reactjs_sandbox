412e83374ba4ad36d4f06274b82b112b421a9def
bslzz <bishalkoid@gmail.com>
2020-04-18 14:47:27 +0300

    edited images and styled css

--- 6_task/src/App.js
+++ 6_task/src/App.js
@@ -6,32 +6,28 @@ const myTexts = [
   {
     id: 1,
     title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, illum similique? Exercitationem perferendis assumenda vitae!',
-    author: 'Bishal Aryal',
+      'Choose your preferred pizza toppings and taste the best flavour in town',
+    author: 'Pizza Palace',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eum et voluptate nisi minima praesentium ex doloribus esse ipsa. Perspiciatis maiores aliquam quae nihil porro quibusdam delectus ipsum nemo quod!',
+      'basil, mushrooms, pepperoncini, artichoke hearts, sun-dried tomatoes, arugula,fresh mozzarella fior di latte, goat cheese, shredded mozzarella, Italian sausage,pepperoni, prosciutto, anchovy',
     image:
-      'https://images.unsplash.com/photo-1503951458645-643d53bfd90f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+      'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=828&q=80',
   },
   {
     id: 2,
-    title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, illum similique? Exercitationem perferendis assumenda vitae!',
-    author: 'Bishal Aryal',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eum et voluptate nisi minima praesentium ex doloribus esse ipsa. Perspiciatis maiores aliquam quae nihil porro quibusdam delectus ipsum nemo quod!',
+    title: 'Variety of burgers and french fries',
+    author: 'Burger Palace',
+    description: 'Small, big, large, medium, children-portion',
     image:
-      'https://images.unsplash.com/photo-1473277319276-325d492fca49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+      'https://cdn.pixabay.com/photo/2019/08/02/15/10/burger-4379863_1280.jpg',
   },
   {
     id: 3,
-    title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, illum similique? Exercitationem perferendis assumenda vitae!',
-    author: 'Bishal Aryal',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eum et voluptate nisi minima praesentium ex doloribus esse ipsa. Perspiciatis maiores aliquam quae nihil porro quibusdam delectus ipsum nemo quod!',
+    title: 'You have not taste the best food yet if you have missed momo',
+    author: 'Momo Palace',
+    description: 'Chicken, Lamb, Fish, Vegetables, Fried, Boiled, Steamed',
     image:
-      'https://images.unsplash.com/photo-1523676060187-f55189a71f5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+      'https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
   },
 ];
 
@@ -45,6 +41,19 @@ class App extends Component {
     oldPosts.splice(removeId, 1);
     this.setState({ posts: oldPosts });
   };
+  clickHandler = (e) => {
+    let likesup = document.querySelector('.far.up');
+    let likesdown = document.querySelector('.far.down');
+    const oldPosts = [...this.state.posts];
+    likesup.classList.toggle('active');
+    likesdown.classList.remove('active');
+  };
+  eventHandler = () => {
+    let likesdown = document.querySelector('.far.down');
+    let likesup = document.querySelector('.far.up');
+    likesdown.classList.toggle('active');
+    likesup.classList.remove('active');
+  };
 
   render() {
     const postList = this.state.posts.map((post, index) => {
@@ -56,10 +65,17 @@ class App extends Component {
           description={post.description}
           image={post.image}
           remove={this.removeHandler.bind(this, index)}
+          like={this.clickHandler.bind(this, index)}
+          dislike={this.eventHandler.bind(this, index)}
         />
       );
     });
-    return <div className="myCards">{postList}</div>;
+    return (
+      <div className="card-samples">
+        <h1>Menu Samples</h1>
+        <div className="myCards">{postList}</div>
+      </div>
+    );
   }
 }
 export default App;
--- 6_task/src/Posts/Post.js
+++ 6_task/src/Posts/Post.js
@@ -6,9 +6,15 @@ const Post = (props) => {
     <div className="cards">
       <div className="card">
         <img src={props.image} alt={props.title} />
-        <h2>{props.title}</h2>
-        <h3>{props.author}</h3>
-        <p>{props.description}</p>
+        <div className="likes">
+          <i class="far up fa-thumbs-up" onClick={props.like}></i>
+          <i class="far down fa-thumbs-down" onClick={props.dislike}></i>
+        </div>
+        <div className="card text">
+          <h2>{props.title}</h2>
+          <h3>{props.author}</h3>
+          <p>{props.description}</p>
+        </div>
         <span>
           <i class="fas fa-times" onClick={props.remove}></i>
         </span>
